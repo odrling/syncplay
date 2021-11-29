23dcd5dc6a7dab75005ed5bd43ae8636eafb66a8
odrling <florianbadie@odrling.xyz>
2021-11-29 01:53:13 +0100

    shuffle is true somehow
    don't ask me how or why

--- syncplay/client.py
+++ syncplay/client.py
@@ -1956,8 +1956,6 @@ class SyncplayPlaylist():
 
         with open(path) as f:
             newPlaylist = f.read().splitlines()
-            if shuffle:
-                random.shuffle(newPlaylist)
             if newPlaylist:
                 self.changePlaylist(newPlaylist, username=None, resetIndex=True)
 
