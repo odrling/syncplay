1ac780baab43e38feff71ad2ae5570c1e63c2746
odrling <florianbadie@odrling.xyz>
2024-01-21 23:08:49 +0100

    server: refuse frequent playlist index updates
    dumb hack, maybe it could at least work

--- syncplay/server.py
+++ syncplay/server.py
@@ -587,6 +587,7 @@ class Room(object):
         self._playlist = []
         self._playlistIndex = None
         self._lastUpdate = time.time()
+        self._lastSetUpdate = time.time()
         self._lastSavedUpdate = 0
         self._position = 0
         self._permanent = False
@@ -699,6 +700,11 @@ class Room(object):
         self.writeToDb()
 
     def setPlaylistIndex(self, index, setBy=None):
+        if time.time() - self._lastSetUpdate < 2:
+            return
+
+        self._lastSetUpdate = time.time()
+
         self._playlistIndex = index
         self.writeToDb()
 
